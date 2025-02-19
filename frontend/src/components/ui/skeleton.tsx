17afc0f69902906341f818e9e453a543680cfd4d
Savakar <savakarrohan@gmail.com>
2025-02-19 23:00:30 +0530

    feat: sidebar component

---
+++ frontend/src/components/ui/skeleton.tsx
@@ -0,0 +1,13 @@
+import { cn } from "@/lib/utils"
+
+function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="skeleton"
+      className={cn("bg-zinc-900/10 animate-pulse rounded-md dark:bg-zinc-50/10", className)}
+      {...props}
+    />
+  )
+}
+
+export { Skeleton }
